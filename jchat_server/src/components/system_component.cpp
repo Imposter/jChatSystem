b5b43314b76c11bd1ad01fc26dac3d3b85c3f3a3
Imposter <scrappycocos@hotmail.com>
2016-04-01 23:26:32 -0400

    Discovered a problem with the Windows build for the client, where the client won't receive any data from the server.

--- jchat_server/src/components/system_component.cpp
+++ jchat_server/src/components/system_component.cpp
@@ -62,6 +62,7 @@ bool SystemComponent::Handle(RemoteChatClient &client, uint16_t message_type,
       return false;
     }
 
+	/*
 	// Get user component
 	UserComponent *user_component = 0;
 	if (!server_->GetComponent(kComponentType_User, user_component)) {
@@ -78,6 +79,7 @@ bool SystemComponent::Handle(RemoteChatClient &client, uint16_t message_type,
 
 	// Set as enabled
 	chat_user->Enabled = true;
+	*/
 
     TypedBuffer send_buffer = server_->CreateBuffer();
     send_buffer.WriteUInt16(kSystemMessageResult_Ok);
