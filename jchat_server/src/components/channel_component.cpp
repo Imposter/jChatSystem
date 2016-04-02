9df1415e32c2e3bf01905483e44a601977dc8ce7
Eyaz Rehman <scrappycocos@hotmail.com>
2016-04-02 13:54:22 -0400

    Fixed disconnection crash in ChannelComponent

--- jchat_server/src/components/channel_component.cpp
+++ jchat_server/src/components/channel_component.cpp
@@ -64,13 +64,13 @@ void ChannelComponent::OnClientDisconnected(RemoteChatClient &client) {
   // disconnected
   channels_mutex_.lock();
   for (auto it = channels_.begin(); it != channels_.end(); ++it) {
-    std::shared_ptr<ChatChannel> &channel = *it;
+    std::shared_ptr<ChatChannel> channel = *it;
 
     if (channel->Enabled) {
       channel->ClientsMutex.lock();
       if (channel->Clients.find(&client) != channel->Clients.end()) {
         // Get the chat user
-        std::shared_ptr<ChatUser> &chat_user = channel->Clients[&client];
+        std::shared_ptr<ChatUser> chat_user = channel->Clients[&client];
 
         // Notify all clients in that channel that the client left
         TypedBuffer clients_buffer = server_->CreateBuffer();
@@ -94,8 +94,13 @@ void ChannelComponent::OnClientDisconnected(RemoteChatClient &client) {
 
         // If there was nobody in the channel delete it
         if (channel->Clients.empty()) {
+          channel->ClientsMutex.unlock();
+          channel->OperatorsMutex.lock();
+          channel->Operators.clear();
+          channel->OperatorsMutex.unlock();
           channel->Enabled = false;
           channel.reset();
+          continue;
         }
       }
       channel->ClientsMutex.unlock();
